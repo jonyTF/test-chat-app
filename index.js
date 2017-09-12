ee4e6c47ad667fbe992b567d67c2f19a2c73ce5f
Jony <liu.z.jonathan@gmail.com>
2017-09-11 19:22:53 -0700

    added rooms and notifications

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 var app = require('express')();
+var room = require('./room.js');
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+app.set('view engine', 'ejs');
+
 app.get('/', function(req, res) {
-	res.sendFile(__dirname + '/index.html');
+	res.render('pages/index');
+});
+
+app.use('/room', room);
+
+app.get('*', function(req, res) {
+	res.send('<h1>Seems you\'ve hit a dead end...</h1>');
 });
 
 io.on('connection', function(socket) {
@@ -11,12 +20,15 @@ io.on('connection', function(socket) {
 	socket.on('disconnect', function() {
 		console.log('user disconnected');
 	});
-	socket.on('chat message', function(msg) {
-		console.log('received chat message');
-		io.emit('chat message', msg);
+	socket.on('join room', function(roomId) {
+		socket.join(roomId);
 	});
-	socket.on('add user', function(nickname) {
-		io.emit('chat message', 'User ' + nickname + ' joined the lobby');
+	socket.on('chat message', function(data) {
+		console.log('received chat message');
+		socket.broadcast.to(data.roomId).emit('chat message', data);
+	});
+	socket.on('add user', function(data) {
+		io.sockets.in(data.roomId).emit('add user', data);
 	});
 });
 
